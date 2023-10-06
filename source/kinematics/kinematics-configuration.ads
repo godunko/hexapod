6b73bc17a637c3becc52ac16e1a3b836e8c50509
Vadim Godunko <vgodunko@gmail.com>
2023-10-06 19:11:51 +0400

    Add values of D parameters for all legs.

--- source/kinematics/kinematics-configuration.ads
+++ source/kinematics/kinematics-configuration.ads
@@ -51,46 +51,64 @@ package Kinematics.Configuration is
 
    --  DH parameters of the frames.
 
-   LF_DH_R1     : constant := 0.029;
    LF_DH_Alpha1 : constant := Ada.Numerics.Pi / 2.0;
-   LF_DH_R2     : constant := 0.084;
+   LF_DH_D1     : constant := 0.0;
+   LF_DH_R1     : constant := 0.029;
    LF_DH_Alpha2 : constant := 0.0;
-   LF_DH_R3     : constant := 0.124;
+   LF_DH_D2     : constant := 0.0;
+   LF_DH_R2     : constant := 0.084;
    LF_DH_Alpha3 : constant := 0.0;
+   LF_DH_D3     : constant := 0.0;
+   LF_DH_R3     : constant := 0.124;
 
-   LM_DH_R1     : constant := 0.029;
    LM_DH_Alpha1 : constant := Ada.Numerics.Pi / 2.0;
-   LM_DH_R2     : constant := 0.084;
+   LM_DH_D1     : constant := 0.0;
+   LM_DH_R1     : constant := 0.029;
    LM_DH_Alpha2 : constant := 0.0;
-   LM_DH_R3     : constant := 0.124;
+   LM_DH_D2     : constant := 0.0;
+   LM_DH_R2     : constant := 0.084;
    LM_DH_Alpha3 : constant := 0.0;
+   LM_DH_D3     : constant := 0.0;
+   LM_DH_R3     : constant := 0.124;
 
-   LR_DH_R1     : constant := 0.029;
    LR_DH_Alpha1 : constant := Ada.Numerics.Pi / 2.0;
-   LR_DH_R2     : constant := 0.084;
+   LR_DH_D1     : constant := 0.0;
+   LR_DH_R1     : constant := 0.029;
    LR_DH_Alpha2 : constant := 0.0;
-   LR_DH_R3     : constant := 0.124;
+   LR_DH_D2     : constant := 0.0;
+   LR_DH_R2     : constant := 0.084;
    LR_DH_Alpha3 : constant := 0.0;
+   LR_DH_D3     : constant := 0.0;
+   LR_DH_R3     : constant := 0.124;
 
-   RF_DH_R1     : constant := 0.029;
    RF_DH_Alpha1 : constant := -Ada.Numerics.Pi / 2.0;
-   RF_DH_R2     : constant := 0.084;
+   RF_DH_D1     : constant := 0.0;
+   RF_DH_R1     : constant := 0.029;
    RF_DH_Alpha2 : constant := 0.0;
-   RF_DH_R3     : constant := 0.124;
+   RF_DH_D2     : constant := 0.0;
+   RF_DH_R2     : constant := 0.084;
    RF_DH_Alpha3 : constant := 0.0;
+   RF_DH_D3     : constant := 0.0;
+   RF_DH_R3     : constant := 0.124;
 
-   RM_DH_R1     : constant := 0.029;
    RM_DH_Alpha1 : constant := -Ada.Numerics.Pi / 2.0;
-   RM_DH_R2     : constant := 0.084;
+   RM_DH_D1     : constant := 0.0;
+   RM_DH_R1     : constant := 0.029;
    RM_DH_Alpha2 : constant := 0.0;
-   RM_DH_R3     : constant := 0.124;
+   RM_DH_D2     : constant := 0.0;
+   RM_DH_R2     : constant := 0.084;
    RM_DH_Alpha3 : constant := 0.0;
+   RM_DH_D3     : constant := 0.0;
+   RM_DH_R3     : constant := 0.124;
 
-   RR_DH_R1     : constant := 0.029;
    RR_DH_Alpha1 : constant := -Ada.Numerics.Pi / 2.0;
-   RR_DH_R2     : constant := 0.084;
+   RR_DH_D1     : constant := 0.0;
+   RR_DH_R1     : constant := 0.029;
    RR_DH_Alpha2 : constant := 0.0;
-   RR_DH_R3     : constant := 0.124;
+   RR_DH_D2     : constant := 0.0;
+   RR_DH_R2     : constant := 0.084;
    RR_DH_Alpha3 : constant := 0.0;
+   RR_DH_D3     : constant := 0.0;
+   RR_DH_R3     : constant := 0.124;
 
 end Kinematics.Configuration;
