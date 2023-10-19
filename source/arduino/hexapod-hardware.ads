06756459f849c547f1ed64876e4ae8d33d70d626
Vadim Godunko <vgodunko@gmail.com>
2023-10-19 11:52:55 +0400

    Siwtch I2C buses.

--- source/arduino/hexapod-hardware.ads
+++ source/arduino/hexapod-hardware.ads
@@ -12,9 +12,9 @@ with BBF.Drivers.PCA9685;
 package Hexapod.Hardware is
 
    Servo_Controller_Left  : aliased BBF.Drivers.PCA9685.PCA9685_Controller
-    (Bus => BBF.Board.I2C.I2C0);
-   Servo_Controller_Right : aliased BBF.Drivers.PCA9685.PCA9685_Controller
     (Bus => BBF.Board.I2C.I2C1);
+   Servo_Controller_Right : aliased BBF.Drivers.PCA9685.PCA9685_Controller
+    (Bus => BBF.Board.I2C.I2C0);
 
    procedure Initialize_Hardware;
    --  Do basic initialization of the hardware.
