f4951b468dedc5a4ee31c46421a9988254144e0a
Vadim Godunko <vgodunko@gmail.com>
2024-09-08 19:31:50 +0400

    Cleanup.

--- source/kinematics/legs-workspace.ads
+++ source/kinematics/legs-workspace.ads
@@ -6,20 +6,9 @@
 
 pragma Restrictions (No_Elaboration_Code);
 
---  with Kinematics;
---  with Reals;
---
 package Legs.Workspace
   with Preelaborate
 is
-
-   --  type Workspace is record
-   --     Center_X : Reals.Real;
-   --     Center_Y : Reals.Real;
-   --     Center_Z : Reals.Real;
-   --     Radius   : Reals.Real;
-   --  end record;
-   --  --  Leg's workspace at ground level.
 
    procedure Compute (Body_Height : Reals.Real);
 
