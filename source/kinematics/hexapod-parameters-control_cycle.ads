0610a7862dc7293dd8f2a0d33963368c92721546
Vadim Godunko <vgodunko@gmail.com>
2024-09-21 21:28:03 +0400

    Parameters of the control loop cycle.

---
+++ source/kinematics/hexapod-parameters-control_cycle.ads
@@ -0,0 +1,13 @@
+--
+--  Copyright (C) 2024, Vadim Godunko
+--
+--  SPDX-License-Identifier: Apache-2.0
+--
+
+package Hexapod.Parameters.Control_Cycle is
+
+   pragma Pure;
+
+   Tick_Duration : constant := 1.0 / 100.0;
+
+end Hexapod.Parameters.Control_Cycle;
---
+++ source/kinematics/hexapod-parameters.ads
@@ -0,0 +1,11 @@
+--
+--  Copyright (C) 2024, Vadim Godunko
+--
+--  SPDX-License-Identifier: Apache-2.0
+--
+
+package Hexapod.Parameters is
+
+   pragma Pure;
+
+end Hexapod.Parameters;
