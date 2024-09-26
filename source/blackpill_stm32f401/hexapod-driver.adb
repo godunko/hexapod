2787dd5a25e255903ab73f05222cddfb850ca68d
Vadim Godunko <vgodunko@gmail.com>
2024-09-26 10:49:12 +0400

    Add tasking library, command line task.

--- source/blackpill_stm32f401/hexapod-driver.adb
+++ source/blackpill_stm32f401/hexapod-driver.adb
@@ -4,12 +4,12 @@
 --  SPDX-License-Identifier: Apache-2.0
 --
 
---  with A0B.ARMv7M.SysTick;
+with A0B.ARMv7M.SysTick;
 --  with A0B.ATSAM3X8E.TC5_Timer;
---  with A0B.Tasking;
---
---  with Debug.Log.Console;
---  with Hexapod.Command_Line;
+with A0B.Tasking;
+
+with Debug.Log.Console;
+with Hexapod.Command_Line;
 --  with Hexapod.Hardware;
 --  with Hexapod.Movement;
 --  --  with Hexapod.Motor_Power_Consumption;
@@ -20,29 +20,24 @@ with Hexapod.Console;
 procedure Hexapod.Driver is
 begin
    Hexapod.Console.Initialize;
+   Debug.Log.Console.Initialize;
 
-   --  Debug.Log.Console.Initialize;
-   --
-   --  A0B.ARMv7M.SysTick.Initialize
-   --   (Use_Processor_Clock => True,
-   --    Clock_Frequency     => 84_000_000);
+   A0B.ARMv7M.SysTick.Initialize
+    (Use_Processor_Clock => True,
+     Clock_Frequency     => 84_000_000);
    --  A0B.ATSAM3X8E.TC5_Timer.Initialize
    --    (Master_Clock_Frequency => 84_000_000,
    --     Source                 => A0B.ATSAM3X8E.TC5_Timer.MCK_32);
-   --
+
    --  Hexapod.Hardware.Initialize_Hardware;
    --  --  Hexapod.Motor_Power_Consumption.Initialize;
    --  Hexapod.Movement.Initialize;
-   --
-   --  A0B.Tasking.Initialize (16#400#);
-   --
-   --  Hexapod.Command_Line.Register_Task;
+
+   A0B.Tasking.Initialize (16#400#);
+
+   Hexapod.Command_Line.Register_Task;
    --  Hexapod.Remote_Control.Register_Task;
    --  Hexapod.Movement.Register_Task;
-   --
-   --  A0B.Tasking.Run;
 
-   loop
-      null;
-   end loop;
+   A0B.Tasking.Run;
 end Hexapod.Driver;
