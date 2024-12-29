5b979cca26670257033c6aee7bcdacd3853448cc
Vadim Godunko <vgodunko@gmail.com>
2024-12-29 17:08:04 +0400

    Improve exit from application on crash of control loop.

--- source/gui/simulation-control_loop.adb
+++ source/gui/simulation-control_loop.adb
@@ -81,115 +81,127 @@ package body Simulation.Control_Loop is
       Aux_Base  : CGK.Primitives.Points_3D.Point_3D;
 
    begin
-      Legs.Initialize;
-      Legs.Workspace.Compute (Scene.Body_Height);
-      Legs.Trajectory.Initialize;
-      Legs.Trajectory_Generator.Initialize;
-      Legs.Gait_Generator.Initialize;
-
-      accept Start;
-
-      Next_Tick := Ada.Real_Time.Clock;
-
       loop
          select
-            accept Stop;
-            exit;
+            accept Start;
+
+            Legs.Initialize;
+            Legs.Workspace.Compute (Scene.Body_Height);
+            Legs.Trajectory.Initialize;
+            Legs.Trajectory_Generator.Initialize;
+            Legs.Gait_Generator.Initialize;
 
          or
-            delay until Next_Tick;
+            accept Stop;
+
+         or
+            terminate;
          end select;
 
-         begin
-            Legs.Trajectory_Generator.Tick;
-            Legs.Gait_Generator.Tick;
+         Next_Tick := Ada.Real_Time.Clock;
 
-         exception
-            when E : others =>
-               Debug.Log.Put_Line (Ada.Exceptions.Exception_Information (E));
-
+         loop
+            select
+               accept Stop;
                exit;
-         end;
 
-         for J in Legs.Leg_Index loop
-            --  Extract legs posture, compute position of joints and end
-            --  effector.
+            or
+               delay until Next_Tick;
+            end select;
 
-            Scene.Legs (J).Posture := Legs.State.Posture (J);
+            begin
+               Legs.Trajectory_Generator.Tick;
+               Legs.Gait_Generator.Tick;
 
-            Legs.Forward_Kinematics
-              (Self     => Legs.Legs (J),
-               Posture  => Scene.Legs (J).Posture,
-               Base     => Aux_Base,
-               Joint_1  => Scene.Legs (J).Joint_1,
-               Joint_2  => Scene.Legs (J).Joint_2,
-               Joint_3  => Scene.Legs (J).Joint_3,
-               Effector => Scene.Legs (J).Effector);
+            exception
+               when E : others =>
+                  Debug.Log.Put_Line
+                    (Ada.Exceptions.Exception_Information (E));
 
-            --  Extract support state
+                  exit;
+            end;
 
-            Scene.Legs (J).Is_Support := Legs.Gait_Generator.Is_Support (J);
+            for J in Legs.Leg_Index loop
+               --  Extract legs posture, compute position of joints and end
+               --  effector.
 
-            --  Extract workspace shapes
+               Scene.Legs (J).Posture := Legs.State.Posture (J);
 
-            Scene.Legs (J).Workspace := Legs.Workspace.Get_Workspace_Shape (J);
+               Legs.Forward_Kinematics
+                 (Self     => Legs.Legs (J),
+                  Posture  => Scene.Legs (J).Posture,
+                  Base     => Aux_Base,
+                  Joint_1  => Scene.Legs (J).Joint_1,
+                  Joint_2  => Scene.Legs (J).Joint_2,
+                  Joint_3  => Scene.Legs (J).Joint_3,
+                  Effector => Scene.Legs (J).Effector);
+
+               --  Extract support state
+
+               Scene.Legs (J).Is_Support := Legs.Gait_Generator.Is_Support (J);
+
+               --  Extract workspace shapes
+
+               Scene.Legs (J).Workspace :=
+                 Legs.Workspace.Get_Workspace_Shape (J);
+            end loop;
+
+            --  Compute movement
+
+            declare
+               P0              : CGK.Primitives.Points_2D.Point_2D :=
+                 CGK.Primitives.Points_2D.Create_Point_2D (X => 0.0, Y => 0.0);
+               P1              : CGK.Primitives.Points_2D.Point_2D :=
+                 CGK.Primitives.Points_2D.Create_Point_2D (X => 1.0, Y => 0.0);
+               V               : CGK.Primitives.Vectors_2D.Vector_2D;
+               Transformation  :
+               CGK.Primitives.Transformations_2D.Transformation_2D;
+
+            begin
+               --  Compute rotation vector
+
+               Legs.Trajectory.Transform
+                 (Legs.Trajectory_Generator.Trajectory.all, P0);
+               Legs.Trajectory.Transform
+                 (Legs.Trajectory_Generator.Trajectory.all, P1);
+
+               V := CGK.Primitives.Vectors_2D.Create_Vector_2D (P0, P1);
+
+               Scene.Ground_Rotate_Z :=
+                 @ + CGK.Reals.Elementary_Functions.Arctan
+                 (X => CGK.Primitives.Vectors_2D.X (V),
+                  Y => CGK.Primitives.Vectors_2D.Y (V));
+
+               --  Compute coordinates offset
+
+               CGK.Primitives.Transformations_2D.Set_Identity (Transformation);
+               CGK.Primitives.Transformations_2D.Rotate
+                 (Transformation, -Scene.Ground_Rotate_Z);
+               CGK.Primitives.Points_2D.Transform (P0, Transformation);
+
+               Scene.Ground_Offset_X := @ + CGK.Primitives.Points_2D.X (P0);
+               Scene.Ground_Offset_Y := @ + CGK.Primitives.Points_2D.Y (P0);
+
+               if Scene.Ground_Offset_X < -0.1 then
+                  Scene.Ground_Offset_X := @ + 0.1;
+
+               elsif Scene.Ground_Offset_X > 0.1 then
+                  Scene.Ground_Offset_X := @ - 0.1;
+               end if;
+
+               if Scene.Ground_Offset_Y < -0.1 then
+                  Scene.Ground_Offset_Y := @ + 0.1;
+
+               elsif Scene.Ground_Offset_Y > 0.1 then
+                  Scene.Ground_Offset_Y := @ - 0.1;
+               end if;
+            end;
+
+            Simulation.Control_Loop.Scene.Set (Scene);
+            Next_Tick :=
+              @ + Ada.Real_Time.To_Time_Span
+              (Hexapod.Parameters.Control_Cycle.Tick_Duration);
          end loop;
-
-         --  Compute movement
-
-         declare
-            P0              : CGK.Primitives.Points_2D.Point_2D :=
-              CGK.Primitives.Points_2D.Create_Point_2D (X => 0.0, Y => 0.0);
-            P1              : CGK.Primitives.Points_2D.Point_2D :=
-              CGK.Primitives.Points_2D.Create_Point_2D (X => 1.0, Y => 0.0);
-            V               : CGK.Primitives.Vectors_2D.Vector_2D;
-            Transformation  :
-              CGK.Primitives.Transformations_2D.Transformation_2D;
-
-         begin
-            --  Compute rotation vector
-
-            Legs.Trajectory.Transform
-              (Legs.Trajectory_Generator.Trajectory.all, P0);
-            Legs.Trajectory.Transform
-              (Legs.Trajectory_Generator.Trajectory.all, P1);
-
-            V := CGK.Primitives.Vectors_2D.Create_Vector_2D (P0, P1);
-
-            Scene.Ground_Rotate_Z :=
-              @ + CGK.Reals.Elementary_Functions.Arctan
-                    (X => CGK.Primitives.Vectors_2D.X (V),
-                     Y => CGK.Primitives.Vectors_2D.Y (V));
-
-            --  Compute coordinates offset
-
-            CGK.Primitives.Transformations_2D.Set_Identity (Transformation);
-            CGK.Primitives.Transformations_2D.Rotate
-              (Transformation, -Scene.Ground_Rotate_Z);
-            CGK.Primitives.Points_2D.Transform (P0, Transformation);
-
-            Scene.Ground_Offset_X := @ + CGK.Primitives.Points_2D.X (P0);
-            Scene.Ground_Offset_Y := @ + CGK.Primitives.Points_2D.Y (P0);
-
-            if Scene.Ground_Offset_X < -0.1 then
-               Scene.Ground_Offset_X := @ + 0.1;
-
-            elsif Scene.Ground_Offset_X > 0.1 then
-               Scene.Ground_Offset_X := @ - 0.1;
-            end if;
-
-            if Scene.Ground_Offset_Y < -0.1 then
-               Scene.Ground_Offset_Y := @ + 0.1;
-
-            elsif Scene.Ground_Offset_Y > 0.1 then
-               Scene.Ground_Offset_Y := @ - 0.1;
-            end if;
-         end;
-
-         Simulation.Control_Loop.Scene.Set (Scene);
-         Next_Tick :=
-           @ + Ada.Real_Time.To_Time_Span
-                 (Hexapod.Parameters.Control_Cycle.Tick_Duration);
       end loop;
    end Robot_Control_Loop;
 
